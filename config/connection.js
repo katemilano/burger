668632871fc6a27914758876d277bcfa332ca56d
Kate Milano <catherine.ann.milano@gmail.com>
2021-03-10 15:03:29 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '12345678',
-  database: 'burger_db',
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '12345678',
+    database: 'burger_db',
+  });
+}
+
 
 connection.connect((err) => {
   if (err) {
